23a5f0636a6ea5e59f3575a78deca6b0653170f0
Afkarinafeb <Afkarina040208@gmail.com>
2025-06-03 13:18:01 +0700

    Delete appinventarisbarang directory

--- appinventarisbarang/inventaris_sanggar/hapus.php
+++
@@ -1,8 +0,0 @@
-<?php
-$koneksi = new mysqli("localhost", "root", "", "dbsanggar");
-
-$id = $_GET['id'];
-$koneksi->query("DELETE FROM peminjaman WHERE id = $id");
-
-header("Location: index.php");
-exit;
